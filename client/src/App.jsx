2603d42df66fac2745156d13b974b6616acd9ace
krishmk1009 <krushnamk1009@gmail.com>
2023-04-10 23:35:04 +0530

    checkup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,95 +5,88 @@ import './App.css'
 
 
 function App() {
-  const [showInput, setShowInput] = useState(false)
-  const [addBtn, setAddBtn] = useState(true)
+  const [showInput, setShowInput] = useState(false);
+  const [addBtn, setAddBtn] = useState(true);
   const [tasks, setTasks] = useState([]);
-  const [inputTask, setInputTask] = useState("")
+  const [inputTask, setInputTask] = useState("");
 
   const userId = "642b35f14c51da2cbc420da4";
 
   const addTask = async (taskName) => {
     try {
-
-      const response = axios.post(`users/${userId}/tasks`, { taskName })
-      setTasks[[...tasks, ...response.data]]
+      const response = await axios.post(`users/${userId}/tasks`, { taskName });
+      setTasks([...tasks, response.data]);
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }
-
-
-  }
+  };
 
   const handleClick = () => {
-    setShowInput(true)
-    setAddBtn(false)
-  }
+    setShowInput(true);
+    setAddBtn(false);
+  };
 
   const handleDoneClick = async () => {
     try {
-      await addTask(inputTask)
-      setInputTask("")
-      setShowInput(false)
+      await addTask(inputTask);
+      setInputTask("");
+      setShowInput(false);
       setAddBtn(true);
-
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }
+  };
 
-  }
   useEffect(() => {
-    const getTask = async () => {
-      const response = await axios.get('/users/642b35f14c51da2cbc420da4/tasks')
-        .then(response => {
-          setTasks(response.data)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    }
+    const getTasks = async () => {
+      try {
+        const response = await axios.get(`/users/${userId}/tasks`);
+        setTasks(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    getTask()
-
-  }, [])
-
-
+    getTasks();
+  }, []);
 
   return (
     <div className="App">
-      <div className='adminProfile'>
-        <h1> Admin's Tasks </h1>
+      <div className="adminProfile">
+        <h1>Admin's Tasks</h1>
       </div>
 
-      <div className='taskList'>
-        {tasks.map(task => {
-          return (
-            <h3>
-              {task.title}
-            </h3>
-          )
+      <div className="taskList">
+        Tasks:
+        {tasks.map((task) => {
+          return <h3>{task.taskName}</h3>; // use 'taskName' instead of 'title'
         })}
-
       </div>
-      <div className='add'>
-        {addBtn && (
-          <button className='add-btn' onClick={handleClick}>
 
+      <div className="add">
+        {addBtn && (
+          <button className="add-btn" onClick={handleClick}>
             Add task +
           </button>
         )}
 
         {showInput && (
-          <div className='input-field'>
-            <input type='text' value={inputTask} onChange={(e) => setInputTask(e.target.value)} />
-            <button className='done-btn' onClick={handleDoneClick}> Done</button>
+          <div className="input-field">
+            <input
+              type="text"
+              value={inputTask}
+              onChange={(e) => setInputTask(e.target.value)}
+            />
+            <button className="done-btn" onClick={handleDoneClick}>
+              Done
+            </button>
           </div>
         )}
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
--- server/index.js
+++ server/index.js
@@ -34,31 +34,47 @@ app.post('/users', async (req, res) => {
     }
 })
 
-  
+
 app.post('/users/:userId/tasks', async (req, res) => {
     try {
         const { userId } = req.params;
         const { taskName } = req.body;
-    
+
         const user = await User.findById(userId);
         if (!user) {
-          return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: 'User not found' });
         }
-    
+
         const task = new Task({ taskName, userId: user._id });
         await task.save();
-    
+
         user.tasks.push(task);
         await user.save();
-    
+
         res.status(201).json(task);
-      } catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server Error' });
-      }
+    }
 })
 
-
+app.get('/users/:userId/tasks', async (req, res) => {
+    try {
+      const userId = req.params.userId;
+  
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json("User not found.");
+      }
+  
+      const tasks = await Task.find({  userId });
+      
+      res.status(200).json(tasks);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
 
 
 app.listen(port, () => {
